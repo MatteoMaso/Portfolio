cb56bb860d504f21fba61220acc45d562eb4c314
matteo.maso <matteo.maso@advertima.com>
2023-03-19 16:41:40 +0100

    refactor: migrate cv style into tailwind

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,16 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
-import { cvStyle, sheets, sheetsContainer, cvContainer} from '../styles/cv.module.css'
+import { sheets } from '../styles/cv.module.css'
 import cv from '../images/cv1.svg'
 import cvPDF from '../images/cv1.pdf'
 import WhiteButton from '../components/whiteButton'
 
 const CV = () => {
     return (
-        <Layout pageTitle="Resume" className={cvContainer}>
-            <div className={sheetsContainer} >
-                <div className={sheets} >
-                    <img loading="lazy" alt="" className={cvStyle} src={cv}></img>
+        <Layout pageTitle="Resume">
+            <div className='container p-9' >
+                <div className={sheets}>
+                    <img loading="lazy" alt="" className='w-full' src={cv}></img>
                 </div>
             </div>
             <div className='w-full bg-blue-700 sticky bottom-0'>
