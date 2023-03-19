06621a3261fee710684ffe55ce9b5a1991f66a88
matteo.maso <matteo.maso@advertima.com>
2023-03-19 16:57:22 +0100

    fix: clickable aread button

--- src/components/whiteButton.js
+++ src/components/whiteButton.js
@@ -3,8 +3,8 @@ import * as React from 'react'
 const WhiteButton = ({btnContent, btnRef }) =>
 {
     return (
-        <div className="m-2 p-1 flex justify-center text-white text-2xl text-center tracking-wider rounded-md border-white border-2 md:w-1/2 w-2/3">
-            <a href={btnRef} className=" hover:shadow-2xl"><h2>{btnContent}</h2></a>
+        <div className="m-2 p-1 flex justify-center text-white text-2xl text-center tracking-wider md:w-1/3 w-2/3">
+            <a href={btnRef} className="hover:shadow-2xl rounded-md border-white border-2 w-full"><h2>{btnContent}</h2></a>
         </div>
     )
 }
