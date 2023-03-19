a904bc139671ded6b9ec235b833c1eb01d89c16b
matteo.maso <matteo.maso@advertima.com>
2023-03-19 15:57:37 +0100

    refactor: add button component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
+import WhiteButton from "../components/whiteButton"
 
 const IndexPage = () => {
   return (
@@ -9,8 +10,8 @@ const IndexPage = () => {
           <div className="md:w-2/5 m-6">
             <h2 className="text-2xl" >Hey there <span role="img" aria-label="hand waveing" >&#128075;</span></h2>
             <p className="text-justify text-xl tracking-wider mt-5 mb-3">
-              Inspired by science to improve people’s life and the
-              environment, I’m a software engineer with a wide interest
+              Inspired by science to improve people's life and the
+              environment, I'm a software engineer with a wide interest
               range and a background in fast prototyping tech solutions.
             </p>
           </div>
@@ -19,11 +20,9 @@ const IndexPage = () => {
 
       <div className="container md:rounded-md  w-screen bg-blue-700 my-5">
         <div className="p-2 grid grid-cols-1 place-items-center">
-          <div className="md:w-2/5 m-6 text-white text-3xl text-center tracking-wider">
+          <div className="md:w-3/5 m-3 text-white text-3xl text-center grid grid-cols-1 place-items-center tracking-wider">
             <h2>Take a look at my career path</h2>
-            <div className="flex justify-center">
-              <a href="/cv" className="border-white border-2 hover:shadow-2xl rounded-md m-5 md:w-1/2 w-2/3 p-1"><h2>CV</h2></a>
-            </div>
+            <WhiteButton btnContent="CV" btnRef="/cv"/>
           </div>
         </div>
       </div>
