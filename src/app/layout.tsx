569f403e94c2d6e72f41eb2d34239922ff948240
matteo.maso <matteo.maso@advertima.com>
2023-10-22 01:10:47 +0200

    fix: low letter on component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header';
+import Header from '@/components/header';
 import '@/styles/globals.css'
 // import AnalyticsProvider from '@/components/analytics'
 import { Analytics } from '@vercel/analytics/react';
