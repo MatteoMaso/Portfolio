199ee711cd29ccbef9bdc73e0cbd87d8c2a0a535
matteo.maso <matteo.maso@advertima.com>
2024-01-13 19:56:35 +0100

    feat: add footer and re wrote about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,28 @@ export const metadata: Metadata = {
 
 export default function HomePage() {
   return (
-    <div>
-      <div className="flex flex-col items-center justify-between">
+    <div className="flex flex-col items-center min-h-screen">
+      <div className="mb-auto">
         <div className="container w-full p-2 mt-1">
           <div className="p-2 grid grid-cols-1 place-items-center">
-            <div className="md:w-2/5 m-6">
-              <h2 className="text-2xl" >Hey there <span role="img" aria-label="hand waveing" >&#128075;</span></h2>
-              <p className="md:text-justify text-xl tracking-wider mt-5 mb-3">
-                Inspired by science to improve people&apos;s life and the
-                environment, I&apos;m a software engineer with a wide interest
-                range and a background in fast prototyping tech solutions.
+            <div className="md:w-2/5 m-6 md:text-left text-xl tracking-wider mt-5 mb-3">
+              <h2 className="text-3xl">Hey there <span role="img" aria-label="hand waveing">&#128075;</span></h2>
+              <p className="py-5">
+                My name is Matteo Maso. 
+                I&apos;m born as a curious problem solver and trained as a software engineer.
               </p>
+              <p className="pb-5">
+                I&apos;m currently developing Backend and Cloud applications 
+                at <a className="text-blue-700" href="https://sensirion-connected.com/" target="_blank" rel="noopener noreferrer">Sensirion Connected Solutions</a>.
+              </p>
+              <p>
+                In life I love using technology to solve meaningful problem and I&apos;m always looking for new challenges.
+              </p>  
             </div>
           </div>
         </div>
 
-        <div className="container md:rounded-md  w-screen bg-blue-700 my-5 sticky bottom-0">
+        <div className="container sm:rounded-md  w-screen bg-blue-700 my-5">
           <div className="p-2 grid grid-cols-1 place-items-center">
             <div className="md:w-3/5 m-3 text-white text-3xl text-center grid grid-cols-1 place-items-center tracking-wider">
               <h2 className="hidden md:block">
@@ -34,6 +40,17 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+
+      {/* Footer */}
+      <footer className="mt-4 w-full flex flex-col items-center justify-between">
+        <div className="border-t border-blue-100 md:w-2/5 w-full">
+          <div className="m-5 md:text-xl text-md">
+            <p className="text-center">
+              <span>&#169;</span> 2024, Matteo Maso. Based in Berlin <span role="img">🇩🇪</span>
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
