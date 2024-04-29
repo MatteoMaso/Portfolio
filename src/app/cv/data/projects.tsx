14079ab6c8f06e95c7c227bc3f6e8f156d6da502
matteo.maso <matteo.maso@advertima.com>
2024-04-29 18:26:25 +0200

    feat: improved page cv components

---
+++ src/app/cv/data/projects.tsx
@@ -0,0 +1,17 @@
+import { Experience } from "@/components/cv/ExperienceItem"
+
+export const projects: Experience[] = [{
+  title: 'Automation Engineer',
+  companyName: 'DaPrette',
+  start: 'Sep 2019',
+  end: 'Feb 2020',
+  location: 'Padova IT',
+  logo: '',
+  children: [
+    'Lectured on Python and machine automation - 150h.',
+    'Built the control system of 2 autonomous food machinery.'
+  ],
+},
+]
+
+
--- src/components/cv/ExperienceItem.tsx
+++ src/components/cv/ExperienceItem.tsx
@@ -1,16 +1,54 @@
-interface Experience {
+import Image from "next/image";
+
+export interface Experience {
   title: string;
+  companyName: string;
   start: string;
   end?: string;
   location?: string;
+  logo: string;
   children: React.ReactNode[];
 }
 
-export default function ExperienceItem({title, start, end, location, children}: Experience) {
+function RenderLogo({ logo }: { logo?: string }) {
+  if (logo === undefined || logo === "") {
+    return null;
+  }
   return (
-    <div className="experienceitem">
-      <h2>{title}</h2>
-      <ul className="list-disc list-inside">{children}</ul>
+    <Image
+      loading="lazy"
+      src={'/logo/' + logo}
+      width={250}
+      height={250}
+      quality={100}
+      alt={"logo"}>
+    </Image>
+  );
+}
+
+export default function ExperienceItem({title, start, end, companyName, location, logo, children}: Experience) {
+  const items = children.map((item, index) => {
+    return <li className="" key={index}>{item}</li>;
+  });
+
+  return (
+    <div className="experienceitem mt-5">
+      <div className='grid md:grid-cols-7 grid-cols-1'>
+        <RenderLogo logo={logo}/>
+        <div className="col-span-6 ml-3">
+          <div className="flex font-medium">
+            <p className="mr-2 text-lg">{title},</p>
+            <p className="text-lg">{companyName}</p>
+          </div>
+          <div className="flex text-gray-500">
+            <p className="mr-3">{start} - {end},</p>
+            <p>{location}</p>
+          </div>
+        </div>
+      </div> 
+      <div>
+        <ul className="list-disc list-inside">{items}</ul>
+      </div>
     </div>
   );
 };
