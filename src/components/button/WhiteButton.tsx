247082b78727c0c3389c223d948e48036357a3fe
matteo.maso <matteo.maso@advertima.com>
2024-05-02 23:39:01 +0200

    fix: clickable area on cv button

--- src/components/button/WhiteButton.tsx
+++ src/components/button/WhiteButton.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface CustomWhiteButtonProps {
     btnContent: string;
     btnRef: string;
@@ -5,8 +7,10 @@ interface CustomWhiteButtonProps {
 
 export function WhiteButton({ btnContent, btnRef }: CustomWhiteButtonProps) {
     return (
-        <div className='text-white text-2xl border-white border-2 p-1.5 rounded-md m-3'>
-            <a className='hover:shadow-2xl' href={btnRef}>{btnContent}</a>
-        </div>
+        <Link href={btnRef}>
+            <div className='text-white text-2xl hover:bg-blue-800  border-white border-2 p-1.5 rounded-md m-3'>
+                {btnContent}
+            </div>
+        </Link>
     );
 }
