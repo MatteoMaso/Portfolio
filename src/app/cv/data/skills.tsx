c09721c9f0afb75baaf6f9fe963d98cb057050d5
matteo.maso <matteo.maso@advertima.com>
2024-05-02 23:37:36 +0200

    refactor: style data skills

--- src/app/cv/data/skills.tsx
+++ src/app/cv/data/skills.tsx
@@ -1,3 +1,17 @@
 export const skills: String[] = [
-  'Typescript','C#','Python','NestJS','PostgreSQL','Swagger docs','openAPI','Jest','e2e-test','MySQL','Blockchain','Git','linux','Docker','Security & Privacy'
+  'Typescript',
+  'C#',
+  'Python',
+  'NestJS',
+  'PostgreSQL',
+  'Swagger docs',
+  'openAPI',
+  'Jest',
+  'e2e-test',
+  'MySQL',
+  'Blockchain',
+  'Git',
+  'linux',
+  'Docker',
+  'Security & Privacy',
 ];
